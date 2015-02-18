5b2d27e70e1676c61ac03f5accbde80a355b5c9c
nikitos <nikitachukov@me.com>
2015-02-19 01:51:44 +0300

    fix recreate script

--- recreate.cmd
+++ recreate.cmd
@@ -1,4 +1,4 @@
-"c:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -h localhost -u root -p < recreate.sql
+"c:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -h localhost -u root --password=admin4all < recreate.sql
 manage.py migrate --noinput
 manage.py loaddata auth
 manage.py loaddata stations
